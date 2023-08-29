dea7f516a9c5683f79ce35dc45a5ca0686926ab0
Anastasiia Prokhorova <naumtasya@gmail.com>
2023-08-29 15:26:46 +0700

    the right decision

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-
-
         int account = 100;
-        int addition = 1000;
-        int oneBonus = 100;
+        int addition = 2000;
+        int finalbonus;
 
-        int finalAccount = account + addition;
-
-        int finalbonus = addition / oneBonus;
-        if (addition > 1000) {
-
-        }
-
-        System.out.println("Всего начислено бонусов: "  + BonusMile + " руб.");
+            if (addition >= 1000) {
+                finalbonus = addition / 100;
+            } else {
+                finalbonus = 0;
+            }
+        int finalAccount = account + addition + finalbonus;
+        System.out.println("Итоговый счёт: "  + finalAccount + " руб.");
     }
 }
