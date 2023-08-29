fc0650d6f9f46cffe1ea3493b9a35ba11bce4e08
Anastasiia Prokhorova <naumtasya@gmail.com>
2023-08-29 14:09:26 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        int account = 100;
+        int addition = 1000;
+        int oneBonus = 100;
+
+        int finalAccount = account + addition;
+
+        int finalbonus = addition / oneBonus;
+        if (addition > 1000) {
+
+        }
+
+        System.out.println("Всего начислено бонусов: "  + BonusMile + " руб.");
+    }
+}
