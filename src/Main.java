e5f5bcedb08e9b7f14b0f153c69f516486083fb5
Anastasiia Prokhorova <naumtasya@gmail.com>
2023-09-06 10:47:27 +0700

    Letter litte and + bonus

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 100;
-        int addition = 2000;
-        int finalbonus;
+        int addition = 2_000;
+        int finalBonus;
 
-        if (addition >= 1000) {
-            finalbonus = addition / 100;
+        if (addition >= 1_000) {
+            finalBonus = addition / 100;
         } else {
-            finalbonus = 0;
+            finalBonus = 0;
         }
-        int finalAccount = account + addition + finalbonus;
+        int finalAccount = account + addition + finalBonus;
+        System.out.println("Бонус: " + finalBonus + " руб.");
         System.out.println("Итоговый счёт: " + finalAccount + " руб.");
     }
 }
